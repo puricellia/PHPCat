16989b39ae12eb3eabb277facaa40628e0153c5e
Harry Traynor <harrytraynor@icloud.com>
2014-10-28 17:48:24 +0000

    Removed pointless echo code to cleanup.

--- index.php
+++ index.php
@@ -10,11 +10,8 @@
 	}
 
 	$cat = new cat('Freddles');
-	$cat->breed(1);
+	$cat->getBreed(1);
 	$cat->getHunger();
 	$cat->getHealth();
-
-	echo "Name: " . $cat->name;
-	echo "Happiness: " . $cat->getHappiness();
 
 ?>
